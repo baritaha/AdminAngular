ded5e39213025fd52b1aa5a68776e862a058ee5a
baritaha <135865393+baritaha@users.noreply.github.com>
2025-04-06 00:46:19 +0300

    Updated user-type.guard.ts with logic for user role validation

--- src/app/guards/user-type.guard.ts
+++ src/app/guards/user-type.guard.ts
@@ -3,21 +3,19 @@ import { users } from '../feature/users/database/users';
 import { showToast } from '../utils/show-toast';
 import { inject } from '@angular/core';
 
-
 export const userTypeGuard: CanActivateFn = (route, state) => {
- const router=inject(Router);
-  const userId = JSON.parse(localStorage.getItem('id')||'');
-  const user = users.find((u) => u.id ===  userId);
-  console.log(userId,user?.name,user?.role);
+  const router = inject(Router);
+  const userId = JSON.parse(localStorage.getItem('id') || '');
+  const user = users.find((u) => u.id === userId);
+  console.log(userId, user?.name, user?.role);
   if (user && user.role === 'admin') {
     return true;
   } else {
     showToast('You are not authorized to access this page', 'red');
     setTimeout(() => {
-   router.navigate(['login']);
-    }
-    , 3000);
-localStorage.clear();
+      router.navigate(['login']);
+    }, 3000);
+    localStorage.clear();
     return false;
   }
 };
